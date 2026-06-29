0837550bd971a4df3fe666c8d8e86eaf923c5713
PKU-Zhou <zch.zhou@outlook.com>
2026-06-29 12:43:31 +0800

    更新脚本

--- scripts/Step2_PowerPlan/power_stripe.tcl
+++ scripts/Step2_PowerPlan/power_stripe.tcl
@@ -27,7 +27,6 @@ addStripe   -nets                               { VDD_MXU VSS_MXU } \
 			-create_pins 1 \
 			-extend_to design_boundary
 
-# 这里增加了 pin 相关的配置
-# 并延伸到了设计边界
+#addStripe -create_pins 1 -extend_to design_boundary
 
 puts "--> 2.4 Add Power Stripe Done."
